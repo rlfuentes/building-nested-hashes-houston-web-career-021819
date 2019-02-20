40a3d00a78cf4d285811abb00190aceb36addca3
Robert <r_lee94@yahoo.com>
2019-02-20 04:19:26 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,5 @@
 def second_challenge
-  epic_tragedy = {
-    montague => "patriarch" => {}, "matriarch" => {}, "hero" => {},"hero_friends" => []
-      }, 
-    capulet =>  "patriarch" => {}, "matriarch" => {},
-    "hero" => {}, "hero_friends" => []
-      }
-  }
+  
 
   
 end
